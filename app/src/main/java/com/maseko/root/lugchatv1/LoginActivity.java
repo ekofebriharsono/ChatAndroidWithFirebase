21d3f2835717b86379add5bfefe4ca33ebbe5694
maseko <ekoaewes2006@gmail.com>
2019-01-22 16:55:18 +0700

    implementation login user with firebase

--- app/src/main/java/com/maseko/root/lugchatv1/LoginActivity.java
+++ app/src/main/java/com/maseko/root/lugchatv1/LoginActivity.java
@@ -1,23 +1,65 @@
 package com.maseko.root.lugchatv1;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edEmail)
+    EditText edEmail;
+
+    @BindView(R.id.edPassword)
+    EditText edPassword;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+        auth = FirebaseAuth.getInstance();
+
     }
 
     @OnClick(R.id.btnLogin)
     public void loginUser() {
-        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+        String txt_email = edEmail.getText().toString();
+        String txt_password = edPassword.getText().toString();
+
+        if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)){
+            Toast.makeText(LoginActivity.this, "All fileds are required", Toast.LENGTH_SHORT).show();
+        } else {
+
+            auth.signInWithEmailAndPassword(txt_email, txt_password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Authentication failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
