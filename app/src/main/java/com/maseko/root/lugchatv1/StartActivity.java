6a5fb22f694228b8862b50b24abf424384dbc3b1
maseko <ekoaewes2006@gmail.com>
2019-01-22 17:58:37 +0700

    create auto login with firebase

--- app/src/main/java/com/maseko/root/lugchatv1/StartActivity.java
+++ app/src/main/java/com/maseko/root/lugchatv1/StartActivity.java
@@ -4,16 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class StartActivity extends AppCompatActivity {
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
         ButterKnife.bind(this);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     @OnClick(R.id.btnLogin)
